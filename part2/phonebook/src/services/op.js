139dd6cdc0577648ecb61a296439867f464ad8f4
mauwu7 <mauricio45nove@gmail.com>
2026-02-06 14:12:34 -0500

    Prueba

--- part2/phonebook/src/services/op.js
+++ part2/phonebook/src/services/op.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/persons';
-const getContacts = () => axios.get(baseUrl).then(response=>response.data);
+const baseUrl = '/api';
+const getContacts = () => axios.get(`${baseUrl}/persons`).then(response=>response.data);
 
-const create = (newContact) => axios.post(baseUrl, newContact).then(response=>response.data);
+const create = (newContact) => axios.post(`${baseUrl}/addPerson`, newContact).then(response=>response.data);
 
-const eliminarContacto = (id) => axios.delete(`${baseUrl}/${id}`);
+const eliminarContacto = (id) => axios.delete(`${baseUrl}/delete/${id}`);
 
 const act = (id, updated) => axios.put(`${baseUrl}/${id}`,updated).then(response=>response.data);
 
---
+++ part3/phonebookBCK/index.js
@@ -0,0 +1,98 @@
+const express=require('express');
+const app=express();
+const morgan=require('morgan');
+const cors=require('cors');
+
+morgan.token('contenido',function getContent(req){
+    return JSON.stringify(req.body)
+})
+
+let contacts = [
+    {
+        id:"1",
+        name: "Arto Hellas",
+        number: "040-1231-213"
+    },
+    {
+        id:"2",
+        name: "Ada Lovelace",
+        number: "12412-214-412"
+    },
+    {
+        id:"3",
+        name: "Dan Abramov",
+        number: "2199-1992-923"
+    },
+    {
+        id:"4",
+        name: "Mary Poppendieck",
+        number: "932-193-1232"
+    }
+
+];
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+app.use(express.static('dist'));
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :contenido'));
+app.use(express.json());
+
+const requestTime = function(req, res, next){
+    const timestamp = Date.now();
+    let time = new Date(timestamp);
+    req.requestTime = time.toUTCString();
+    next();
+}
+app.use(requestTime);
+
+app.get('/api/info',(req,res) =>{
+    let responseText = 
+    `<p> Phonebook has info for ${contacts.length} people</p>
+     <p>${req.requestTime}<p/>`;
+    res.send(responseText);
+});
+
+app.post('/api/addPerson', (req, res)=>{
+    const person = req.body;
+    if('name' in person && 'number' in person){
+        if(contacts.find(contact => contact.name === person.name) === undefined){
+            person.id = String(getRandomInt(10000));
+            contacts.push(person);
+            res.json(req.body);
+        }
+        else{
+            res.status(409).json({error: 'name must be unique'});
+        }
+    }
+    else{
+        res.status(400).json({error: 'content missing'});
+    }
+    
+});
+
+app.get('/api/persons', (req, res) =>{
+    res.json(contacts);
+});
+
+app.get('/api/persons/:id', (req,res)=>{
+    const person = contacts.find((cont) => req.params.id === cont.id);
+    if(person){
+        res.json(person);
+    }
+    else{
+        res.status(404).end();
+    }
+});
+
+app.delete('/api/delete/:id', (req,res) => {
+    contacts = contacts.filter(person => person.id !== req.params.id);
+    res.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
